cdb7bf8296aebcf7d17f5af0700b4636bf4c620b
kshitija <kbhosale975@gmail.com>
2022-12-26 13:56:55 +0530

    added one sop in 2

--- src/com/velocity/demo1/demo1.java
+++ src/com/velocity/demo1/demo1.java
@@ -1,5 +1,7 @@
-public class demo{
+package com.velocity.demo1;
+public class demo1{
 public static void main(String[] args) {
 System.out.println("demo");
+System.out.println("dev-02 in scenario 2");
 }
 }
